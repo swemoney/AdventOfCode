8d16332f88a79cdb245dfc42eddb6988140175f8
swemoney <tehdnite@gmail.com>
2023-12-19 23:00:05 -0600

    Cleaned up directions a bit

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from dataclasses import dataclass
 
 # Various methods that I could probably use in multiple puzzles
 
@@ -8,7 +9,33 @@ class DotDict(dict):
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
 
+# Named tuple for a point/coordinate (y, x)
 Point = namedtuple("Point", ["y","x"])
+
+@dataclass
+class Direction:
+    coords: Point
+    name: str
+    short: str
+
+    @property
+    def y(self):
+        return self.coords.y
+    
+    @property
+    def x(self):
+        return self.coords.x
+
+class Directions:
+    north = Direction(coords=Point(-1, 0), name="North", short="N")
+    northeast = Direction(coords=Point(-1, 1), name="North East", short="NE")
+    east = Direction(coords=Point(0, 1), name="East", short="E")
+    southeast = Direction(coords=Point(1, 1), name="South East", short="SE")
+    south = Direction(coords=Point(1, 0), name="South", short="S")
+    southwest = Direction(coords=Point(1, -1), name="South West", short="SW")
+    west = Direction(coords=Point(0, -1), name="West", short="W")
+    northwest = Direction(coords=Point(-1, -1), name="North West", short="NW")
+
 DIRECTIONS = {
     "N": Point(-1, 0),
     "NE": Point(-1, 1),
