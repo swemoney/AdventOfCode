b5a88a10710893ef7226992a30980b6a58bb836c
swemoney <tehdnite@gmail.com>
2023-12-26 12:11:47 -0600

    Refactored some of these for consistency. Going to break some solutions.

--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
-from collections import namedtuple
-from dataclasses import dataclass
+from dataclasses import dataclass, fields
 
 # Various methods that I could probably use in multiple puzzles
 
@@ -9,46 +8,60 @@ class DotDict(dict):
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
 
-# Named tuple for a point/coordinate (y, x)
-Point = namedtuple("Point", ["y","x"])
+@dataclass(frozen=True)
+class Vector2:
+    x: int
+    y: int
 
-@dataclass
+    def __add__(self, other):
+        return type(self)(*[getattr(self, f.name) + getattr(other, f.name) for f in fields(self)])
+    
+    def __sub__(self, other):
+        return type(self)(*[getattr(self, f.name) - getattr(other, f.name) for f in fields(self)])
+    
+    def __mul__(self, other):
+        return type(self)(*[getattr(self, f.name) * getattr(other, f.name) for f in fields(self)])
+    
+    def __div__(self, other):
+        return type(self)(*[getattr(self, f.name) / getattr(other, f.name) for f in fields(self)])
+
+@dataclass(frozen=True)
+class Vector3(Vector2):
+    z: int
+
+DIRECTIONS = {
+    "N": Vector2(0, -1),
+    "NE": Vector2(1, -1),
+    "E": Vector2(1, 0),
+    "SE": Vector2(1, 1),
+    "S": Vector2(0, 1),
+    "SW": Vector2(-1, 1),
+    "W": Vector2(-1, 0),
+    "NW": Vector2(-1, -1)
+}
+
+@dataclass(frozen=True)
 class Direction:
-    coords: Point
+    coords: Vector2
     name: str
     short: str
 
-    @property
-    def y(self):
-        return self.coords.y
-    
-    @property
-    def x(self):
-        return self.coords.x
-
+    def __getattr__(self, attr):
+        if attr in ["x","y"]:
+            return getattr(self.coords, attr)
+        return self.coords
+   
 class Directions:
-    north = Direction(coords=Point(-1, 0), name="North", short="N")
-    northeast = Direction(coords=Point(-1, 1), name="North East", short="NE")
-    east = Direction(coords=Point(0, 1), name="East", short="E")
-    southeast = Direction(coords=Point(1, 1), name="South East", short="SE")
-    south = Direction(coords=Point(1, 0), name="South", short="S")
-    southwest = Direction(coords=Point(1, -1), name="South West", short="SW")
-    west = Direction(coords=Point(0, -1), name="West", short="W")
-    northwest = Direction(coords=Point(-1, -1), name="North West", short="NW")
-
-DIRECTIONS = {
-    "N": Point(-1, 0),
-    "NE": Point(-1, 1),
-    "E": Point(0, 1),
-    "SE": Point(1, 1),
-    "S": Point(1, 0),
-    "SW": Point(1, -1),
-    "W": Point(0, -1),
-    "NW": Point(-1, -1)
-}
+    north = Direction(coords=DIRECTIONS["N"], name="North", short="N")
+    northeast = Direction(coords=DIRECTIONS["NE"], name="North East", short="NE")
+    east = Direction(coords=DIRECTIONS["E"], name="East", short="E")
+    southeast = Direction(coords=DIRECTIONS["SE"], name="South East", short="SE")
+    south = Direction(coords=DIRECTIONS["S"], name="South", short="S")
+    southwest = Direction(coords=DIRECTIONS["SW"], name="South West", short="SW")
+    west = Direction(coords=DIRECTIONS["W"], name="West", short="W")
+    northwest = Direction(coords=DIRECTIONS["NW"], name="North West", short="NW")
 
 def clamp(n, min, max):
     if n < min: return min
     elif n > max: return max
     else: return n 
-
